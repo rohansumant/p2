a55e52261d1a28cafe81e827a8bd2435104fba5b
Rohan Sumant <r.s.sumant@gmail.com>
2017-10-01 11:14:59 -0700

    Starting anew

--- scheduling.c
+++ scheduling.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "priority_queue.h"
+
 
 #define NUMBER_OF_PROCS 10
 #define RUNS_PER_ALGO 5
@@ -11,6 +11,20 @@
 // why does arrival time need to be a float?
 // why does expected run time run from 0.1 to 10
 //     with possible values: [ 0.1, 1, 2, 3, 4 .. 10 ]
+
+typedef struct {
+    float arrival_time;
+    float expected_runtime;
+    int priority;
+} processf;
+
+
+typedef struct {
+    int pid;
+    int arrival_time;
+    float expected_runtime;
+    int priority;
+} process;
 
 
 int compare_arrival_times(const void * a, const void * b)
@@ -57,55 +71,31 @@ int fcfs(process *ptr)
 }
 
 // Shortest Job First (non-preemptive)
-
-int cmp_arrival_time_expected_runtime(const void* p1, const void* p2) {
-    int at1 = ((process*)p1)->arrival_time, at2 = ((process*)p2)->arrival_time;
-    if(at1 == at2) return ((process*)p1)->expected_runtime < ((process*)p2)->expected_runtime;
-    return at1 < at2;
-}
-
-int sjf(process *arr) {
-    qsort(arr, NUMBER_OF_PROCS, sizeof(process), cmp_arrival_time_expected_runtime);
-    int time[QUANTA+15];
-    int curr_time = 0;
-    for(int i=0;i<NUMBER_OF_PROCS;i++) {
-	for(int j=curr_time;j<curr_time+arr[i].expected_runtime;j++) time[j] = arr[i].pid;
-	curr_time += arr[i].expected_runtime;
+int sjf(process *ptr)
+{
+// sort on expected runtimes
+    int i;
+    qsort(ptr, NUMBER_OF_PROCS, sizeof(process), compare_expected_runtimes);
+    for(i=0; i<NUMBER_OF_PROCS; i++)
+    {
+        printf("pid: %d , expected runtime: %f\n", ptr[i].pid, ptr[i].expected_runtime);
     }
-    for(int i=0;i<curr_time;i++) {
-	printf("Current time quanta:%d PID:%d\n",i,time[i]);
-    }
+
     return 0;
 }
 
 // Shortest Remaining Time (preemptive)
-
-float key_expected_runtime(process p) {
-    return p.expected_runtime;
-}
-
-int srt(process *arr) {
-    qsort(arr, NUMBER_OF_PROCS, sizeof(process), compare_arrival_times);
-    int ptr = 0;
-    int i = 0;
-    priority_queue Q;
-    init(&Q);
-    while(ptr != NUMBER_OF_PROCS || size(&Q) != 0) {
-	while(ptr != NUMBER_OF_PROCS && arr[ptr].arrival_time == i) {
-	    push(&Q,arr[ptr],key_expected_runtime);
-	    ptr++;
-	}
-	if(size(&Q)) {
-	    process p = peek(&Q);
-	    printf("Current time quanta:%d PID:%d\n",i,p.pid);
-	    pop(&Q,key_expected_runtime);
-	    if(p.expected_runtime > 1.0) {
-		p.expected_runtime -= 1.0;
-		push(&Q,p,key_expected_runtime);
-	    }
-	}
-	i++;
+int srt(process *ptr)
+{
+// sort on shortest remaining time
+    int i;
+    qsort(ptr, NUMBER_OF_PROCS, sizeof(process), compare_expected_runtimes);
+    for(i=0; i<NUMBER_OF_PROCS; i++)
+    {
+        printf("pid: %d , expected runtime: %f\n", ptr[i].pid, ptr[i].expected_runtime);
     }
+
+    return 0;
 }
 
 // Round Robin (preemptive)
@@ -162,7 +152,7 @@ int main()
     process proc_list[NUMBER_OF_PROCS];
     int i;
     process *buff = malloc(NUMBER_OF_PROCS * sizeof(process));
-/*
+
     printf("Running fcfs %d times\n", RUNS_PER_ALGO);
     for (i=0; i< RUNS_PER_ALGO; i++)
     {
@@ -170,7 +160,7 @@ int main()
         generate_procs(buff);
         fcfs(buff);
     }
-*/
+
     printf("Running sjf %d times\n", RUNS_PER_ALGO);
     for (i=0; i< RUNS_PER_ALGO; i++)
     {
@@ -178,17 +168,15 @@ int main()
         generate_procs(buff);
         sjf(buff);
     }
+
     printf("Running srt %d times\n", RUNS_PER_ALGO);
     for (i=0; i< RUNS_PER_ALGO; i++)
     {
         printf("%d\n", i);
         generate_procs(buff);
-	for(int j=0;j<NUMBER_OF_PROCS;j++) {
-	    printf("%d %d %f\n",buff[j].pid,buff[j].arrival_time,buff[j].expected_runtime);
-	}
         srt(buff);
     }
-/*
+
     printf("Running rr %d times\n", RUNS_PER_ALGO);
     for (i=0; i< RUNS_PER_ALGO; i++)
     {
@@ -204,5 +192,5 @@ int main()
         generate_procs(buff);
         hpf(buff);
     }
-*/
+
 }
